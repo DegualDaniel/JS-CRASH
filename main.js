baa2d618637c62ff3e527b61310d0f6b0226edcc
Degual <97003832+DegualDaniel@users.noreply.github.com>
2025-06-18 14:42:42 +0100

    Add files via upload
    1ST JS

---
+++ main.js
@@ -0,0 +1,219 @@
+// const name = ("Daniel")
+// const z = true
+// console.log(name)
+
+// var lastName = ('Degualz')
+
+
+// console.log(lastName + 'daniel' +'awa')
+// console.log(typeof(lastName))
+// console.log(-(10-2))
+
+
+// const person = {
+//     firstBorn : 'Degual',
+//     LastChild : 'esther',
+//     age : 30 ,
+//     married : true,
+
+// }
+
+// console.log(person.firstBorn)
+// console.log(person.LastChild)
+// console.log(person.married)
+
+
+
+
+// const oddNumber = [1, 3, 5, 7, 9]
+// console.log(oddNumber[1])
+
+// const isEven = (10 % 2 === 0 ? 'it is true' : 'it is false ')
+// console.log(isEven)
+
+// const x = 2
+// const y = 10
+
+// console.log(y !== x ? true : false)
+
+// // CONDITIONALS statement 
+
+// const num = 0
+
+// if( num > 0){
+//     console.log('number  is a valid number')
+// }else if(num < 0){
+//     console.log('number is negative')
+// } else {
+//     console.log('number is zero')
+// }
+
+
+
+
+
+
+
+
+// const gender = 'male'
+// const middleName = 'daniel'
+// const age = 30 
+// const married = false
+
+// console.log(middleName.length)
+// console.log(age)
+// console.log(gender.charAt(3))
+
+
+
+
+
+
+
+// const a =2
+// const b=3 
+// const c = 4 
+// const d= 5 
+
+// const ans =(2*d**3 / b**2)
+// console.log(ans)
+
+
+
+// // math 
+// const ab = Math.random() * 100000
+// // .floor
+// const ac = Math.floor(Math.random() * 100000)
+// console.log(ac)
+
+// const bb = Math.random() * 10000
+// const bd = Math.floor(bb)
+// console.log(bd)
+
+
+
+// // concatination
+// const nickName = 'degual'
+// const agee = 50 
+
+// console.log('my  name ' + nickName + ' and i am ' + agee + ' years')
+
+// // `${} ` es-6 way of concatination
+
+// // ARRAYS 
+// const f = [1,2,3,4,5,67,7,8,9 ]
+// const ff = ["degual", 33, "daniel"]
+// console.log(ff)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const jss1 = ['Degual', 'Daniel', 'Esther', 'Chima', 'Emmanuel']
+const jss2 = ['Ava', 'Liam', 'Sophia', 'Dorcas', 'Naoh']
+const jss3 = ['Olivia', 'Ethan', 'Mia', 'Lucas', 'Isabella']
+const ss1 = ['Mason', 'Charlotte', 'Logan', 'Amelia', 'William']
+const ss2 = ['Harper', 'Benjamin', 'Evelyn', 'Caleb', 'Abigail']
+const ss3 = ['Rock', 'Victoria', 'Jessica', 'Alex', 'James']
+
+// JSS1
+console.log(jss1)
+// SELECTING INDEX IN ARRAY
+console.log(jss1[0])
+// ADDING A VALUE TO ARRAY
+console.log(jss1.push('Liam'))
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(jss1)
+// REMOVING A VALUE IN THE ARRAY 
+console.log(jss1.shift())
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(jss1)
+
+
+
+
+
+// JSS2
+console.log(jss2)
+// SELECTING INDEX IN ARRAY
+console.log(jss2[2])
+// ADDING A VALUE TO ARRAY
+console.log(jss2.unshift('Jason'))
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(jss2)
+// REMOVING A VALUE IN THE ARRAY 
+console.log(jss2.shift())
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(jss2)
+
+
+
+
+// JSS3
+console.log(jss3)
+console.log(jss3[3])
+// ADDING A VALUE TO ARRAY
+console.log(jss3.unshift('Stella'))
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(jss3)
+// REMOVING A VALUE IN THE ARRAY 
+console.log(jss3.shift())
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(jss3)
+
+
+
+// SS1
+console.log(ss1)
+console.log(ss1[4])
+// ADDING A VALUE TO ARRAY
+console.log(ss1.unshift('Bolu'))
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(ss1)
+// REMOVING A VALUE IN THE ARRAY 
+console.log(ss1.shift())
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(ss1)
+
+
+
+// SS2
+console.log(ss2)
+console.log(ss2[3])
+// ADDING A VALUE TO ARRAY
+console.log(ss2.unshift('Bella'))
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(ss2)
+// REMOVING A VALUE IN THE ARRAY 
+console.log(ss2.shift())
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(ss2)
+
+
+// SS3
+console.log(ss3)
+console.log(ss3[2])
+// ADDING A VALUE TO ARRAY
+console.log(ss3.unshift('Ella'))
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(ss3)
+// REMOVING A VALUE IN THE ARRAY 
+console.log(ss3.shift())
+// DISPLAYING ALL VALUES IN ARRAY
+console.log(ss3)
+
