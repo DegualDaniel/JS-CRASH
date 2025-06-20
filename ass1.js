258536531d56f12b24404474027a05b8d33246f6
Degual <97003832+DegualDaniel@users.noreply.github.com>
2025-06-20 15:59:19 +0100

    Add files via upload
    loop and conditional assignment

---
+++ ass1.js
@@ -0,0 +1,205 @@
+
+// CONDITIONALS ASSIGNMENT
+// const a = prompt('put in a score');
+const a = 20
+
+if( a >= 70 ){
+ console.log('Excellent')
+}else if( a >= 60 && a <=69 ){
+ console.log('Very Good')
+}else if( a >= 50 && a <=59 ){
+     console.log('Good')
+}else if( a >= 40 && a <=49 ){
+     console.log('pass')
+}else if( a >= 30 || a <=39 ){
+     console.log('Fail')   
+}
+
+
+
+
+// LOOP ASSIGNMENT
+
+const jss1 = ['Degual', 'Daniel', 'Esther', 'Chima', 'Emmanuel']
+const jss2 = ['Ava', 'Liam', 'Sophia', 'Dorcas', 'Naoh']
+const jss3 = ['Olivia', 'Ethan', 'Mia', 'Lucas', 'Isabella']
+const ss1 = ['Mason', 'Charlotte', 'Logan', 'Amelia', 'William']
+const ss2 = ['Harper', 'Benjamin', 'Evelyn', 'Caleb', 'Abigail']
+const ss3 = ['Rock', 'Victoria', 'Jessica', 'Alex', 'James']
+
+
+
+// FOR LOOP JSS1 
+for(let b = 0; b < jss1.length; b++){
+     console.log(jss1[b])
+}
+
+// FOR LOOP JSS2 
+for(let b = 0; b < jss2.length; b++){
+     console.log(jss2[b])
+}
+
+// FOR LOOP JSS3
+for(let b = 0; b < jss3.length; b++){
+     console.log(jss3[b])
+}
+
+// FOR LOOP SS1 
+for(let b = 0; b < ss1.length; b++){
+     console.log(ss1[b])
+}
+
+
+// FOR LOOP SS2 
+for(let b = 0; b < ss2.length; b++){
+     console.log(ss2[b])
+}
+
+
+// FOR LOOP SS3
+for(let b = 0; b < ss3.length; b++){
+     console.log(ss3[b])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// LAST ASSIGNMENT 
+
+// const jss1 = ['Degual', 'Daniel', 'Esther', 'Chima', 'Emmanuel']
+// const jss2 = ['Ava', 'Liam', 'Sophia', 'Dorcas', 'Naoh']
+// const jss3 = ['Olivia', 'Ethan', 'Mia', 'Lucas', 'Isabella']
+// const ss1 = ['Mason', 'Charlotte', 'Logan', 'Amelia', 'William']
+// const ss2 = ['Harper', 'Benjamin', 'Evelyn', 'Caleb', 'Abigail']
+// const ss3 = ['Rock', 'Victoria', 'Jessica', 'Alex', 'James']
+
+// // JSS1
+// console.log(jss1)
+// // SELECTING INDEX IN ARRAY
+// console.log(jss1[0])
+// // ADDING A VALUE TO ARRAY
+// console.log(jss1.push('Liam'))
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(jss1)
+// // REMOVING A VALUE IN THE ARRAY
+// console.log(jss1.shift())
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(jss1)
+
+// // JSS2
+// console.log(jss2)
+// // SELECTING INDEX IN ARRAY
+// console.log(jss2[2])
+// // ADDING A VALUE TO ARRAY
+// console.log(jss2.unshift('Jason'))
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(jss2)
+// // REMOVING A VALUE IN THE ARRAY
+// console.log(jss2.shift())
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(jss2)
+
+// // JSS3
+// console.log(jss3)
+// console.log(jss3[3])
+// // ADDING A VALUE TO ARRAY
+// console.log(jss3.unshift('Stella'))
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(jss3)
+// // REMOVING A VALUE IN THE ARRAY
+// console.log(jss3.shift())
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(jss3)
+
+// // SS1
+// console.log(ss1)
+// console.log(ss1[4])
+// // ADDING A VALUE TO ARRAY
+// console.log(ss1.unshift('Bolu'))
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(ss1)
+// // REMOVING A VALUE IN THE ARRAY
+// console.log(ss1.shift())
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(ss1)
+
+// // SS2
+// console.log(ss2)
+// console.log(ss2[3])
+// // ADDING A VALUE TO ARRAY
+// console.log(ss2.unshift('Bella'))
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(ss2)
+// // REMOVING A VALUE IN THE ARRAY
+// console.log(ss2.shift())
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(ss2)
+
+// // SS3
+// console.log(ss3)
+// console.log(ss3[2])
+// // ADDING A VALUE TO ARRAY
+// console.log(ss3.unshift('Ella'))
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(ss3)
+// // REMOVING A VALUE IN THE ARRAY
+// console.log(ss3.shift())
+// // DISPLAYING ALL VALUES IN ARRAY
+// console.log(ss3)
